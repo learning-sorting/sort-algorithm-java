fb08cae9319afaf106d89fb3a6b1b28207acb68f
Vishal Kumar Singh <singhvishalkumar997@gmail.com>
2020-10-24 14:25:11 +0530

    Aesthetic

--- SelectionSortPedro.java
+++ SelectionSortPedro.java
@@ -1,10 +1,14 @@
-public class SelectionSortPedro {  
-    public static void selectionSort(int[] arr){  
+public class SelectionSortPedro
+{
+    public static void selectionSort(int[] arr)
+    {
         for (int i = 0; i < arr.length - 1; i++)  
         {  
             int index = i;  
-            for (int j = i + 1; j < arr.length; j++){  
-                if (arr[j] < arr[index]){  
+            for (int j = i + 1; j < arr.length; j++)
+            {
+                if (arr[j] < arr[index])
+                {
                     index = j;//searching for lowest index  
                 }  
             }  
@@ -14,19 +18,20 @@ public class SelectionSortPedro {
         }  
     }  
        
-    public static void main(String a[]){  
+    public static void main(String a[])
+    {
         int[] arr1 = {9,14,3,2,43,11,58,22};  
         System.out.println("Before Selection Sort");  
-        for(int i:arr1){  
+        for(int i:arr1)
             System.out.print(i+" ");  
-        }  
+
         System.out.println();  
           
         selectionSort(arr1);
          
         System.out.println("After Selection Sort");  
-        for(int i:arr1){  
+        for(int i:arr1)
             System.out.print(i+" ");  
-        }  
+
     }  
 }  
